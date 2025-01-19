f516995b99f37f68898187a87ed01cd6cf1c2817
Deaponn <bartek.sajo@interia.pl>
2025-01-19 13:55:25 +0100

    Implement API calls to YouTube

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,16 +3,84 @@ import HomeIcon from "@/assets/icons/home-icon.svg";
 import SearchIcon from "@/assets/icons/search-icon.svg";
 import { fontStyles } from "@/constants/FontStyles";
 import { Colors } from "@/constants/Colors";
+import { createContext, useEffect, useState } from "react";
+
+export const VideosContext = createContext<AllTopics<VideoResponseData[]>>({
+  reactNative: [],
+  react: [],
+  typescript: [],
+  javascript: [],
+});
+
+type Topic = "reactNative" | "react" | "typescript" | "javascript";
+
+type AllTopics<T> = {
+  [key in Topic]: T;
+};
+
+const topics: AllTopics<string> = {
+  reactNative: "React Native",
+  react: "React",
+  typescript: "Typescript",
+  javascript: "Javascript",
+};
+
+interface VideoResponseData {
+  id: {
+    videoId: string;
+  };
+  snippet: {
+    publishedAt: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    channelTitle: string;
+  };
+}
 
 export default function TabsLayout() {
+  const [videosData, setVideosData] = useState<AllTopics<VideoResponseData[]>>({
+    reactNative: [],
+    react: [],
+    typescript: [],
+    javascript: [],
+  });
+
+  useEffect(() => {
+    async function fetchVideos() {
+      const allResults: AllTopics<VideoResponseData[]> = {
+        reactNative: [],
+        react: [],
+        typescript: [],
+        javascript: [],
+      };
+      for (const [key, topic] of Object.entries(topics)) {
+        const queryUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.EXPO_PUBLIC_API_KEY}&q=${topic}&type=video`;
+        const response = await fetch(queryUrl);
+        const json = await response.json();
+        allResults[key as keyof AllTopics<any>] = json.items as VideoResponseData[];
+      }
+      setVideosData(allResults);
+    }
+
+    fetchVideos();
+  }, []);
+
   return (
+    <VideosContext.Provider value={videosData}>
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.accent,
         tabBarInactiveTintColor: Colors.light,
         tabBarStyle: {
           backgroundColor: Colors.background,
-          height: 72
+          height: 72,
         },
         tabBarLabelStyle: fontStyles.poppinsRegular16,
         headerShown: false,
@@ -33,5 +101,6 @@ export default function TabsLayout() {
         }}
       />
     </Tabs>
+    </VideosContext.Provider>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,13 @@
 import { Link } from "expo-router";
+import { useContext } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { VideosContext } from "@/app/(tabs)/_layout";
 
 export default function Index() {
+  const videos = useContext(VideosContext);
+
+  if (videos.react.length === 0) return <><Text>Loading...</Text></>
+
   return (
     <View
       style={{
@@ -10,7 +16,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Main view of the app.</Text>
+      <Text>Main view of the app. {videos.javascript.length} {videos.typescript[0].snippet.title}</Text>
       <Link href="/video/1">View first video details</Link>
       <Link href="/video/2">View second video details</Link>
     </View>
